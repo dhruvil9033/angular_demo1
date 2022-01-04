38119c8bec89cb635903355b245285476eea78eb
dhruv7200 <rohitgajera72@gmail.com>
2022-01-04 17:23:20 +0530

    04 jan 2022

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  // styleUrls: ['./app.component.css']
-  styles:[`
-    h3{
-      color:red;
-    }
-  `]
+  styleUrls: ['./app.component.css']
+  // styles:[`
+  //   /*h3{*/
+  //   /*  color:red;*/
+  //   /*}*/
+  // `]
 })
 export class AppComponent {
   title = '';
+  oddNumbers:number[] = [];
+  evenNumbers:number[] = [];
+
+  onIntervalFired(firedNumber: number){
+    if(!firedNumber){
+      this.evenNumbers.push(firedNumber);
+    }else {
+      this.oddNumbers.push(firedNumber);
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,9 @@ import { WarningComponent } from './warning/warning.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
 import { PracticeComponent } from './practice/practice.component';
 import { Practice2Component } from './practice2/practice2.component';
+import { OddComponent } from './odd/odd.component';
+import { EvenComponent } from './even/even.component';
+import { GameControlComponent } from './game-control/game-control.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +22,10 @@ import { Practice2Component } from './practice2/practice2.component';
     WarningComponent,
     SuccessAlertComponent,
     PracticeComponent,
-    Practice2Component
+    Practice2Component,
+    OddComponent,
+    EvenComponent,
+    GameControlComponent
   ],
   imports: [
     BrowserModule,
